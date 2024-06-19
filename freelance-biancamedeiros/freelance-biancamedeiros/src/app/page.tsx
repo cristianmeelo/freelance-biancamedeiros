a86393f74cc3e409357dbccfd123e26dbd3edc6e
cristianmeelo <cristianmeelo@gmail.com>
2024-06-19 00:41:42 -0300

    try

--- freelance-biancamedeiros/freelance-biancamedeiros/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Header } from './components/header';
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
-      <Header />
-    </main>
-  );
-}
