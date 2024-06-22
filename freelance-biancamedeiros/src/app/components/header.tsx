c6478d73a8fe2c7e79ab9a46e70ae6951111955a
cristianmeelo <cristianmeelo@gmail.com>
2024-06-21 22:10:16 -0300

    feat - Development of header

--- freelance-biancamedeiros/src/app/components/header.tsx
+++ freelance-biancamedeiros/src/app/components/header.tsx
@@ -1,3 +1,14 @@
 export const Header = () => {
-  return <header>Bianca</header>;
+  return (
+    <header className="fixed top-0 left-0 w-full px-8 flex justify-between bg-red-50 z-50">
+      <div>Logo</div>
+      <ul className="flex  space-x-10 font-kandira">
+        <li>Home</li>
+        <li>Designer</li>
+        <li>Projetos</li>
+        <li>Consultoria</li>
+        <li>Contato</li>
+      </ul>
+    </header>
+  );
 };
--- freelance-biancamedeiros/tailwind.config.ts
+++ freelance-biancamedeiros/tailwind.config.ts
@@ -8,13 +8,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        kandira: ['Kandira', 'sans-serif'],
       },
+      colors: {
+        base_light: "#EBDED7",
+      }
     },
   },
   plugins: [],
 };
+
 export default config;
