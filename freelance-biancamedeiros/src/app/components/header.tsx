8b9ce5861f152d15d06fe5293eae731e9188998b
cristianmeelo <cristianmeelo@gmail.com>
2024-06-21 23:57:52 -0300

    bug: fixed bug hover

--- freelance-biancamedeiros/src/app/components/header.tsx
+++ freelance-biancamedeiros/src/app/components/header.tsx
@@ -2,18 +2,18 @@ import Image from 'next/image';
 import { useState } from 'react';
 import logo from '../../../public/logo.svg';
 
+const DEFAULT_HOVERED = -1;
 export const Header = () => {
   const links = [
-    { text: 'Home', color: 'base_light' },
-    { text: 'Designer', color: 'base_orange' },
-    { text: 'Projetos', color: 'base_red' },
-    { text: 'Consultoria', color: 'base_green' },
-    { text: 'Contato', color: 'base_light' },
+    { text: 'Home', url: '#' },
+    { text: 'Designer', url: 'base_orange' },
+    { text: 'Projetos', url: 'base_red' },
+    { text: 'Consultoria', url: 'base_green' },
+    { text: 'Contato', url: 'base_light' },
   ];
 
-  const DEFAULT_HOVERED = -1;
   const [linkHovered, setLinkHovered] = useState(DEFAULT_HOVERED);
-  const [outMenu, setOutMenu] = useState(true);
+  const [outMenu, setOutMenu] = useState(false);
 
   return (
     <header className="fixed top-0 left-0 w-full h-20 px-8 flex justify-between items-center bg-red-50 z-50">
@@ -24,18 +24,30 @@ export const Header = () => {
           width={200}
         />
       </div>
-      <ul className="flex space-x-10 text-2xl font-kandira" onMouseEnter={()=> setOutMenu(true)} onMouseLeave={()=> setOutMenu(false)}>
+      <ul
+        className="flex space-x-10 text-2xl font-kandira"
+        onMouseEnter={() => setOutMenu(true)}
+        onMouseLeave={() => setOutMenu(false)}
+      >
         {links.map((link, index) => (
           <li
             key={index}
-            onMouseEnter={()=> setLinkHovered(index)}
-            onMouseLeave={()=> setLinkHovered(DEFAULT_HOVERED)}
+            onMouseEnter={() => setLinkHovered(index)}
+            onMouseLeave={() => setLinkHovered(DEFAULT_HOVERED)}
             className={`relative cursor-pointer transition-colors duration-300 
-              ${index === linkHovered ? 'text-base-orange' : !outMenu ? 'text-black' : 'text-base_light'} 
+              ${
+                index === linkHovered
+                  ? 'text-base-orange'
+                  : !outMenu
+                  ? 'text-black'
+                  : 'text-base_light'
+              }
 
-                      `}          >
-            <span className="relative z-10">{link.text}</span>
-    
+                      `}
+          >
+            <a href={link.url} className="relative z-10">
+              {link.text}
+            </a>
           </li>
         ))}
       </ul>
